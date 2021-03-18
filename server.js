64f1cdba4bfc39f4a42d637ce7dd03b44c2baf58
Valentin Giselbrecht <vgiselbrecht@hotmail.com>
2021-03-18 21:01:32 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server, {cors:{origin: '*',}});
+
+server.listen(process.env.PORT || 8080);
+
+io.on('connection', function (socket) {
+    socket.on('login', function (data) {
+        socket.join(data.room);
+        socket.join(data.room + "-" + data.id);
+    });
+    socket.on('push', function (data) {
+        if(data.receiver != 0){
+            socket.to(data.room + "-" + data.receiver).emit('pull', data);
+        }else{
+            socket.to(data.room).emit('pull', data);
+        }
+    });
+});
